cfc95fa73a5e312a80dc679ded011340e4ee415d
Shivakumar Ranade <G705762@genmills.com>
2025-12-01 18:29:15 +0530

    updated code to convert to jsonl as per gemini 2.5 fine tuning format

--- Preprocess/convert.py
+++ Preprocess/convert.py
@@ -1,6 +1,10 @@
 def convert_to_jsonl(incident):
     inc = {}
-    inc["input_text"] = "Classify the following incident into a service offering." + "\n\n" + "Short Description:" + "\n" + incident["Short description"].replace('\n', '') + "\n\n" + "Long Description" + "\n" + incident["Description"].replace('\n', '') + "\n\n" + "Service Offering:"
-    inc["output_text"] = incident["Service offering"]
+    input_text = "Classify the following incident into a service offering." + "\n\n" + "Short Description:" + "\n" + incident["Short description"].replace('\n', '') + "\n\n" + "Long Description" + "\n" + incident["Description"].replace('\n', '') + "\n\n" + "Service Offering:"
+    output_text = incident["Service offering"]
+
+    inc["contents"] = []
+    inc["contents"].append({"role": "user", "parts" : [{"text": input_text}]})
+    inc["contents"].append({"role": "model", "parts" : [{"text": output_text}]})
 
     return inc
